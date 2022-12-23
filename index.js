11fc68e96593e7462a124ad4be9c3ad4c16a96f0
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2022-12-23 19:18:39 +0800

    update index.js

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
+app.get('/', (req, res) => {
+    res.send("Server is running")
+})
 app.use('/openai', require('./route/openAiRoutes'))
 
 app.listen(port, ()=> {
